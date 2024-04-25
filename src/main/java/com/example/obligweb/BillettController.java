bf4eee79f3ae556a0eee1e983b2985dfed5c1da4
jemeh7802 <jemeh7802@oslomet.no>
2024-04-25 21:47:46 +0200

    Oblig 3 fullført

---
+++ src/main/java/com/example/obligweb/BillettController.java
@@ -0,0 +1,39 @@
+package com.example.obligweb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BillettController {
+    @Autowired
+    private BillettRepository rep;
+
+    @PostMapping("/lagre")
+    public void lagreBillett(@RequestBody Billett billett) {
+        rep.lagreBilletter(billett);
+    }
+
+    @GetMapping("/hentAlle")
+    public List<Billett> hentAlleBilletter() {
+        return rep.hentAlleBilletter();
+    }
+
+    @GetMapping("/hentEn")
+    public Billett hentEnBillett(@RequestParam Integer id) {
+        return rep.hentEnBillett(id);
+    }
+
+
+    @GetMapping("/slettEn")
+    public void slettEn(Integer id) {
+        rep.slettEn(id);
+    }
+
+
+    @DeleteMapping("/slettAlleBilletter")
+    public void slettAlle() {
+        rep.slettAlle();
+    }
+}
