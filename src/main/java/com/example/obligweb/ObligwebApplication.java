0e384aeb65fa9af15967b63e50deb33975b99b76
jemeh7802 <jemeh7802@oslomet.no>
2024-04-25 19:06:33 +0200

    Initial commit

---
+++ src/main/java/com/example/obligweb/ObligwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.obligweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObligwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obligweb/ObligwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obligweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
